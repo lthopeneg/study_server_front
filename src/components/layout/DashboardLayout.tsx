a4e751ca638bd0593361ce4426760757e7ff732b
임태환 <lth.openeg@gmail.com>
2026-06-04 16:29:46 +0900

    fix: 사용하지 않는 React import 제거(TS6133 에러 해결)

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, Link, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../../store/useAuthStore';
 import { api } from '../../services/api';
